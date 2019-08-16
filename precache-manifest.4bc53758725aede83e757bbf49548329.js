24752b5b63f3968bb3255f0e8d299ab948be4ebb
Giseokkang <river.key93@gmail.com>
2019-08-16 12:08:46 +0900

    Updates

---
+++ precache-manifest.4bc53758725aede83e757bbf49548329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d5d129487cba4892be495bd6cd9359",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "878c35a99a7a1a4ad9d9",
+    "url": "/nomflix/static/js/2.20e7073a.chunk.js"
+  },
+  {
+    "revision": "77b4a3837971929b813f",
+    "url": "/nomflix/static/js/main.af4d5f65.chunk.js"
+  },
+  {
+    "revision": "a5c4e370f881ffb2ccde",
+    "url": "/nomflix/static/js/runtime~main.963d735e.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
